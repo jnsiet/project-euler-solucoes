f66abcf35754799e9050c3d96ad8ab0909b29551
João Paulo <joaopaulo30011@gmail.com>
2023-12-25 17:26:48 -0300

    feat: adiciona solução do desafio 16

---
+++ solucoes/desafio-016.py
@@ -0,0 +1,13 @@
+""""
+Soma de dígitos de potência
+2^15 = 32768 e a soma de seus algarismos é 3 + 2 + 7 + 6 + 8 = 26.
+Qual é a soma dos algarismos do número 2^1000?
+"""
+
+numero = pow(2, 1000)
+soma_digitos = 0
+
+for digito in str(numero):
+    soma_digitos += int(digito)
+
+print(f"RESPOSTA: {soma_digitos}")
