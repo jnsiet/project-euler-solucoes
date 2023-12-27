081781d57d04b859c6ab137350502fecfa0b0574
João Paulo <joaopaulo30011@gmail.com>
2023-12-27 16:39:02 -0300

    feat: adiciona solução do desafio 48

---
+++ solucoes/desafio-048.py
@@ -0,0 +1,14 @@
+"""
+>> Poderes próprios
+
+As séries, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Encontre os últimos dez dígitos da série, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+"""
+
+soma = 0
+
+for numero in range(1, 1001):
+    soma += pow(numero, numero)
+
+print(f"RESPOSTA: {str(soma)[-10:]}")
