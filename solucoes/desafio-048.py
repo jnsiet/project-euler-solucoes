023f1dac51b837ffc18d5d9f82be814b1b3f07c4
João Paulo <joaopaulo30011@gmail.com>
2023-12-27 16:46:40 -0300

    style: melhora descrição do desafio

--- solucoes/desafio-048.py
+++ solucoes/desafio-048.py
@@ -1,7 +1,7 @@
 """
 >> Poderes próprios
 
-As séries, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+A série de potências, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 
 Encontre os últimos dez dígitos da série, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 """
