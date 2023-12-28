40f957fd6d6a9602dea83d0f7a71ab1d739db7ed
João Paulo <joaopaulo30011@gmail.com>
2023-12-28 06:50:52 -0300

    feat: adiciona solução do desafio 29

---
+++ solucoes/desafio-029.py
@@ -0,0 +1,27 @@
+"""
+>> Poderes distintos
+
+Considere todas as combinações inteiras de a^b para 2 ≤ a ≤ 5 e 2 ≤ b ≤ 5:
+
+            2^2 = 4,    2^3 = 8,    2^4 = 16,   2^5 = 32
+            3^2 = 9,    3^3 = 27,   3^4 = 81,   3^5 = 243
+            4^2 = 16,   4^3 = 64,   4^4 = 256,  4^5 = 1024
+            5^2 = 25,   5^3 = 125,  5^4 = 625,  5^5 = 3125
+
+Se eles forem colocados em ordem numérica, com quaisquer repetições removidas, obteremos a seguinte sequência de 15
+termos distintos:
+
+            4, 8, 9, 16, 25, 27, 32, 64, 81, 125, 243, 256, 625, 1024, 3125.
+
+Quantos termos distintos existem na sequência gerada por a^b para 2 ≤ a ≤ 100 e 2 ≤ b ≤ 100?
+"""
+
+numeros = []
+
+for base in range(2, 101):
+    for expoente in range(2, 101):
+        resultado = pow(base, expoente)
+        if resultado not in numeros:
+            numeros.append(resultado)
+
+print(f"RESPOSTA: {len(numeros)}")
